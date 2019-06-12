ff5db55c66e6c2aa5312f54abacf221ae0a39c56
jan <jan@kwoh.de>
2019-06-12 22:22:12 +0200

    Bump version: 0.1.14 → 0.1.15

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     name='lspace',
     packages=find_packages(),
     include_package_data=True,
-    version='0.1.14',
+    version='0.1.15',
     entry_points='''
         [console_scripts]
         lspace=lspace.app:cli_group
