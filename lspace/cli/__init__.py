9f23113742147cb4f4b8f0a90037e55183cb580c
jan <jan@kwoh.de>
2019-06-16 20:04:39 +0200

    rm print

--- lspace/cli/__init__.py
+++ lspace/cli/__init__.py
@@ -13,6 +13,5 @@ from .reimport_command import reimport
 from .export_command import export
 from .version_command import version
 
-print(os.environ.get('LSPACE_DEV', None))
 if os.environ.get('LSPACE_DEV', None) == '1':
     from .db_command import db
