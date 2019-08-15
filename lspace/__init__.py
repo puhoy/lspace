36324a5dc7c27df34768963a231c11b0fa33cd43
jan <jan@kwoh.de>
2019-08-15 17:14:20 +0200

    Bump version: 0.3.0 → 0.4.0

--- lspace/__init__.py
+++ lspace/__init__.py
@@ -1,6 +1,6 @@
 import flask
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 import logging
 import os
--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     name='lspace',
     packages=find_packages(),
     include_package_data=True,
-    version='0.3.0',
+    version='0.4.0',
     entry_points='''
         [console_scripts]
         lspace=lspace.app:cli_group
