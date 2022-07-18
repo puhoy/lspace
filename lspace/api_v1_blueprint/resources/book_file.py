628174928f6536b6e8c559fb181f73cb88f93c29
jan <jan@kwoh.de>
2022-07-19 01:11:28 +0200

    update readme

--- lspace/api_v1_blueprint/resources/book_file.py
+++ lspace/api_v1_blueprint/resources/book_file.py
@@ -3,6 +3,10 @@ from flask_restx import Resource
 from flask import send_file
 from lspace.models import Book
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class BookFile(Resource):
 
@@ -13,6 +17,7 @@ class BookFile(Resource):
             title_slug=book.title_slug,
             extension=book.extension
         )
+        logger.debug(f"sending {book.full_path}")
         return send_file(book.full_path,
                          as_attachment=True,
                          attachment_filename=attachment_filename)
